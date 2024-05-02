36eb1bb77cf57a6d1c8f84d88665786602525e70
Zerumi <afanasyev09032005@gmail.com>
2024-05-02 23:08:38 +0300

    feat: init gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    application
+    kotlin("jvm") version "1.9.23"
+}
+
+group = "io.github.zerumi"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "io.github.zerumi.MainKt"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.github.ajalt.clikt:clikt:4.4.0")
+    implementation("com.indvd00m.ascii.render:ascii-render:2.2.0")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
